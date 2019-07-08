6b402c306b104b55af7fb81a885acc5399b8f995
Legaeldan <legaeldan@gmail.com>
2019-07-09 00:45:59 +0100

    Added fade in function to circles.

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -1,10 +1,10 @@
-
 $(document).ready( function() {
     $("#game-title").fadeTo(2000, 1);
     $("#start-game").delay(1500).fadeTo(2000, 1);
 
     $("#start-game").on("click", function () {
-        $(this).fadeTo(1500, 0);
+        $(this).fadeOut(1500, 0);
+        $("li:hidden").delay(1500).fadeIn(1500);
     });
     
 
