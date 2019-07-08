241eaf4504909b79c569ee9c54e6c12f5b00727a
Legaeldan <legaeldan@gmail.com>
2019-07-08 23:14:34 +0100

    Added fade to start button on click.

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -1,4 +1,14 @@
 
 $(document).ready( function() {
     $("#game-title").fadeTo(2000, 1);
+    $("#start-game").delay(1500).fadeTo(2000, 1);
+
+    $("#start-game").on("click", function () {
+        $(this).fadeTo(1500, 0);
+    });
+    
+
 });
+
+
+
