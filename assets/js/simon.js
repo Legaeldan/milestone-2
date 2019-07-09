36fd35baf999d43b41c98070547d7bc44fea8b54
Cunnane <kieran.cunnane@abbott.com>
2019-07-09 11:36:18 +0100

    Added sounds to buttons.

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -6,9 +6,34 @@ $(document).ready( function() {
         $(this).fadeOut(1500, 0);
         $("li:hidden").delay(1500).fadeIn(1500);
     });
-    $("li").on("click", function () {
+    $("li").on("mousedown", function () {
         $(this).toggleClass("jqhover");
+    }).on("mouseup", function () {
+        $(this).toggleClass("jqhover");
+    })
+
+    //sound files defined below
+    var simonSound = document.createElement("audio");
+    simonSound.volume = 1;
+
+    $(".red-circle").click(function () {
+        simonSound.src = "assets/sounds/simonSound1.mp3";
+        simonSound.play();
     });
+
+    $(".green-circle").click(function () {
+        simonSound.src = "assets/sounds/simonSound2.mp3";
+        simonSound.play();
+    });
+    $(".yellow-circle").click(function () {
+        simonSound.src = "assets/sounds/simonSound3.mp3";
+        simonSound.play();
+    });
+    $(".blue-circle").click(function () {
+        simonSound.src = "assets/sounds/simonSound4.mp3";
+        simonSound.play();
+    });
+
 
 });
 
