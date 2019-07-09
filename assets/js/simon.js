1ac7cf65de7a44512180e1ccd252f2176241f5a9
Cunnane <kieran.cunnane@abbott.com>
2019-07-09 14:38:14 +0100

    Added clicker function with fadeIn

--- assets/js/simon.js
+++ assets/js/simon.js
@@ -1,22 +1,24 @@
-var totalClicks = 0;
-
-
 $(document).ready( function() {
     $("#game-title").fadeTo(2000, 1);
     $("#start-game").delay(1500).fadeTo(2000, 1);
+    
 
     $("#start-game").on("click", function () {
         $(this).fadeOut(1500, 0);
         $("li:hidden").delay(1500).fadeIn(1500);
+        $(".clicker").delay(1500).fadeIn(1500);
     });
+
     $("li").on("mousedown", function () {
         $(this).toggleClass("jqhover");
     }).on("mouseup", function () {
         $(this).toggleClass("jqhover");
     })
-
-    $(".four-buttons").on("click", function () {
+ 
+    var totalClicks = 0;
+    $(".four-buttons").on("click", function clicked() {
         totalClicks++;
+        document.getElementById("clickCounter").innerHTML = totalClicks;
     })
 
     //sound files defined below
@@ -41,7 +43,7 @@ $(document).ready( function() {
         simonSound.play();
     });
 
-});
 
+});
 
 
