01e26eaee2e7e88444be1a379f5ab2cc28b27024
Legaeldan <legaeldan@gmail.com>
2019-07-19 23:19:10 +0100

    Turned if statement on computerTurn into one function.

---
+++ assets/js/game.js
@@ -0,0 +1,156 @@
+//Document ready function to fade in title and start
+$(document).ready(function () {
+    $("#game-title").fadeIn(2000);
+    $("#logo").delay(1500).fadeIn(2000).delay(500).fadeOut(2000);
+    $("#start-game").delay(5500).fadeIn(2000);
+    $(".speedControl").delay(5500).fadeIn(2000, 0);
+});
+
+const simonSound = document.createElement("audio");
+const easy = 1000;
+const medium = 600;
+const hard = 300;
+const possibleMoves = ['red', 'green', 'yellow', 'blue'];
+var totalClicks = 0;
+var movesMade = [];
+var playerMoves = [];
+var randomNum = 0;
+var tempo = easy;
+var duration = 0;
+
+$(".speedControl").on("click", function () {
+    $(".speedControl").removeClass("btn-danger").addClass("btn-success");
+    $(this).addClass("btn-danger").removeClass("btn-success");
+    tempo = eval(this.id);
+    console.log(tempo);
+});
+
+function randomise() {
+    //generates random number
+    randomNum = Math.floor(Math.random() * 4);
+    //Adds random number to array
+    movesMade.push(randomNum);
+    console.log(movesMade);
+    console.log(randomNum);
+};
+
+
+//Brings start button and title into view
+$("#start-game").on("click", function () {
+    simonSound.volume = 1;
+    duration = (movesMade.length * tempo);
+    totalClicks = 0;
+    document.getElementById("clickCounter").innerHTML = totalClicks;
+    $(this).fadeOut(1500);
+    $(".speedControl").fadeOut(1500);
+    $("li").delay(1500).fadeIn(1500);
+    $(".clicker").delay(1500).fadeIn(1500);
+    if (window.innerWidth < 768) {
+        $("#clickCounter").fadeOut(0);
+        $(".clickHeader").fadeOut(0);
+    }
+    document.getElementById("levelNo").innerHTML = "Level:1";
+    $("#levelNo").delay(1500).fadeIn(1500);
+    randomise();
+    $("#waiting").delay(1500).fadeIn(1500).delay(duration).fadeOut(500);
+    $("#playing").delay((duration) + 3000).fadeIn(1500);
+    setTimeout(function () {
+        computerTurn();
+    }, 3000);
+});
+
+function compEachTurn(tempo, i, color) {
+setTimeout(function () {
+    $("#"+color).delay(tempo * i).toggleClass("jqhover");
+    playSound(color);
+    setTimeout(function () {
+        $("#"+color).toggleClass("jqhover");
+    }, tempo / 2);
+}, tempo * i);
+};
+
+//plays each iteration in array, but sound causes break
+function computerTurn() {
+    document.getElementById("levelNo").innerHTML = "Level:" + (movesMade.length);
+    playerMoves = [];
+    duration = (tempo * movesMade.length)
+    $("#simon-main").addClass("disableInput");
+    for (let i = 0; i < movesMade.length; i++) {
+        const colour = movesMade[i];
+        const endColour = possibleMoves[colour];
+        compEachTurn (tempo, i, endColour)
+        setTimeout(function () {
+            $("#simon-main").removeClass("disableInput");
+        }, duration);
+        console.log(possibleMoves[colour]);
+        console.log(colour);
+        //  }, 500 * movesMade.indexOf(colour) + 1);
+    };
+};
+
+$(".four-buttons").on("click", function () {
+    playerMoves.push(possibleMoves.indexOf(this.id));
+    var lastMove = playerMoves.length - 1;
+    if (playerMoves.length == movesMade.length && playerMoves[lastMove] == movesMade[lastMove]) {
+        totalClicks++;
+        duration = (movesMade.length * tempo);
+        document.getElementById("clickCounter").innerHTML = totalClicks;
+        $("#playing").fadeOut(500).delay(duration + 1000).fadeIn(1500);
+        $("#waiting").fadeIn(1500).delay(duration).fadeOut(500);
+        randomise();
+        setTimeout(function () {
+            computerTurn();
+        }, 1000);
+    } else if (playerMoves[lastMove] == movesMade[lastMove]) {
+        totalClicks++;
+        document.getElementById("clickCounter").innerHTML = totalClicks;
+    } else if (playerMoves[lastMove] !== movesMade[lastMove] && totalClicks == 0) {
+        $("#gameOver").modal();
+        document.getElementById("gameOverModal").innerHTML = "Unlucky! You got " + totalClicks + "! Try again and maybe this time you can try and get at least " + (totalClicks + 1) + "?!";
+        clearGame();
+    } else {
+        $("#gameOver").modal();
+        document.getElementById("gameOverModal").innerHTML = "Congratulations! You got " + totalClicks + "! Try again and maybe this time you can get " + (totalClicks + 1) + "!";
+        clearGame();
+    };
+    console.log(playerMoves);
+});
+
+//Clears the current game and resets the clicker to 0
+function clearGame() {
+    simonSound.volume = 0;
+    $(".fourbuttons").children().stop(true, true).toggleClass("jqhover");
+    $(".fourbuttons").children().removeClass("jqhover");
+    $("li").fadeOut(1500);
+    $(".clicker").fadeOut(1500);
+    $("#levelNo").fadeOut(1500);
+    $("#playing").fadeOut(1500)
+    $("#start-game").delay(1500).fadeIn(1500, 0);
+    $(".speedControl").delay(1500).fadeIn(1500, 0);
+    movesMade = [];
+    playerMoves = [];
+};
+
+
+
+//reset button tied to clearGame function
+$("#reset").on("click", clearGame);
+
+//Toggle the light up function when clicked
+$("li").on("mousedown touchstart", function () {
+    $(this).addClass("jqhover");
+}).on("mouseup touchend", function () {
+    $(this).removeClass("jqhover");
+}).on("mouseout", function () {
+    $(this).removeClass("jqhover");
+});
+
+
+$(".four-buttons").on("click", function () {
+    playSound(this.id);
+});
+
+function playSound(color){
+    simonSound.src="assets/sounds/"+color+"Sound.mp3"
+    simonSound.play();
+}
